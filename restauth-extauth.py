1c6e5f8eccbe603c2b1f977df0a51433cecd3b3d
Mathias Ertl <mati@er.tl>
2014-08-12 22:42:19 +0200

    use standard python3 interpreter

--- restauth-extauth.py
+++ restauth-extauth.py
@@ -1,28 +1,30 @@
-#!/home/mati/git/restauth/RestAuthClient/py3/bin/python
+#!/usr/bin/env python3
 
 from __future__ import print_function
 
 import os
 import sys
 
-os.environ['PYTHONPATH'] = '/home/mati/git/restauth/RestAuthCommon/python'
-sys.path.insert(0, '/home/mati/git/restauth/RestAuthCommon/python')
 from configparser import ConfigParser
+config = ConfigParser({
+    'PYTHONPATH': '',
+})
+config.read([
+    '/etc/restauth-extauth.conf',
+    os.path.expanduser(os.path.join('~', '.restauth-extauth.conf')),
+    os.path.join(os.path.dirname(sys.argv[0]), 'restauth-extauth.conf'),
+])
+section = os.environ.get('CONTEXT', 'restauth')
+
+pythonpath = config.get(section, 'PYTHONPATH')
+if pythonpath:
+    sys.path = pythonpath.split(':') + sys.path
 
 from RestAuthClient.restauth_user import User
 from RestAuthClient.common import RestAuthConnection
 
 username = sys.stdin.readline().strip("\n")
 line2 = sys.stdin.readline().strip("\n")
-
-config = ConfigParser()
-config.read([
-    '/etc/restauth-extauth.conf',
-    os.path.expanduser(os.path.join('~', '.restauth-extauth.conf')),
-    os.path.join(os.path.dirname(sys.argv[0]), 'restauth-extauth.conf'),
-])
-
-section = os.environ.get('CONTEXT', 'restauth')
 
 conn = RestAuthConnection(
     config.get(section, 'server'),
