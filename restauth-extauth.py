471f4b072269d07c0234cfc06843b47916eb6c98
Mathias Ertl <mati@fsinf.at>
2014-08-15 15:50:29 +0200

    implement redis caching for password

--- restauth-extauth.py
+++ restauth-extauth.py
@@ -16,7 +16,11 @@ line2 = sys.stdin.readline().strip("\n")
 
 # Read configuration
 config = ConfigParser({
-    'PYTHONPATH': '',
+    'PYTHONPATH': None,
+    'cache': None,
+    'redis-server': 'localhost',
+    'redis-port': '6379',
+    'redis-db': '0',
 })
 config.read([
     '/etc/restauth-extauth.conf',
@@ -27,8 +31,58 @@ section = os.environ.get('CONTEXT', 'restauth')
 
 # Append any python path
 pythonpath = config.get(section, 'PYTHONPATH')
-if pythonpath:
+if pythonpath is not None:
     sys.path = pythonpath.split(':') + sys.path
+
+###################
+### Redis cache ###
+###################
+class RedisCache(object):
+    def __init__(self, config, section):
+        import redis
+        self.conn = redis.StrictRedis(
+            config.get(section, 'redis-server'),
+            config.getint(section, 'redis-port'),
+            config.getint(section, 'redis-db')
+        )
+
+    def check_password(self, user, password):
+        """Check the given user and password.
+
+        Returns None on cache miss, True if password matches, False if not.
+        """
+        cached = self.conn.get('%s-pass' % user)
+        print('Got from cache: "%s", checking "%s"' % (cached, password), file=sys.stderr)
+        if cached is None:
+            return cached
+        else:
+            return cached == bytes(password, 'utf-8')
+
+    def set_password(self, user, password):
+        self.conn.set('%s-pass' % user, password)
+
+    def in_group(self, user, group):
+        pass
+
+# Find out if we should check a password or a group membership
+authtype = os.environ.get('AUTHTYPE', 'PASS').lower()
+
+# Setup cache
+cache = config.get(section, 'cache')
+if cache is not None:
+    if cache == 'redis':
+        cache = RedisCache(config, section)
+    else:
+        print('Unknown cache "%s".', file=sys.stderr)
+        sys.exit(1)
+
+    if authtype == 'pass':
+        checked = cache.check_password(username, line2)
+        if checked is True:
+            sys.exit(0)
+        elif checked is False:
+            sys.exit(1)
+        # else: cache miss
 
 # Setup RestAuth connection
 conn = RestAuthConnection(
@@ -38,12 +92,16 @@ conn = RestAuthConnection(
 )
 user = User(conn, username)
 
-authtype = os.environ.get('AUTHTYPE', 'PASS').lower()
 if authtype == 'pass':
     if user.verify_password(line2):
+        # set in cache if defined
+        if cache is not None:
+            cache.set_password(username, line2)
+
         sys.exit(0)
     else:
         sys.exit(1)
+
 elif authtype == 'group':
     if user.in_group(line2):
         sys.exit(0)
