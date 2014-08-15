76e66c899383c07af5e4b0b6ade9cf944d037841
Mathias Ertl <mati@fsinf.at>
2014-08-15 22:46:06 +0200

    add common hashing function

--- restauth-extauth.py
+++ restauth-extauth.py
@@ -2,7 +2,9 @@
 
 from __future__ import print_function
 
+import hashlib
 import os
+import random
 import sys
 
 from configparser import ConfigParser
@@ -23,6 +25,7 @@ config = ConfigParser({
     'redis-port': '6379',
     'redis-db': '0',
     'memcache-server': '127.0.0.1:11211',
+    'password-hash': '',
 })
 config.read([
     '/etc/restauth-extauth.conf',
@@ -30,6 +33,7 @@ config.read([
     os.path.join(os.path.dirname(sys.argv[0]), 'restauth-extauth.conf'),
 ])
 section = os.environ.get('CONTEXT', 'restauth')
+crypt_algo = config.get(section, 'password-hash')
 
 # Append any python path
 pythonpath = config.get(section, 'PYTHONPATH')
@@ -42,10 +46,46 @@ if pythonpath is not None:
 #######################
 class CacheBase(object):
     expire = config.getint(section, 'cache-expire')
+    _bcrypt = None
 
     def prefix(self, key):
         return 'authnz-external:%s:%s' % (section, key)
 
+    def _salt(self, length=12):
+        chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+        return ''.join([random.choice(chars) for i in range(length)])
+
+    def _hash_bcrypt(self, password, salt=None):
+        if self._bcrypt is None:
+            import bcrypt
+            self._bcrypt = bcrypt
+
+        if salt is None:
+            return bcrypt.hashpw(password, bcrypt.gensalt())
+        else:
+            return bcrypt.hashpw(password, salt)
+
+    def _hash_hashlib(self, password, salt=None):
+        if salt is None:
+            salt = self._salt()
+        else:
+            salt = salt.split('$', 1)[0]
+
+        to_hash = bytes('%s$%s' % (salt, password), 'utf-8')
+        return '%s$%s' % (salt, getattr(hashlib, crypt_algo)(to_hash).hexdigest())
+
+    def _hash_none(self, password, salt=None):
+        return password
+
+    if crypt_algo == 'bcrypt':
+        hash = _hash_bcrypt
+    elif hasattr(hashlib, crypt_algo):
+        hash = _hash_hashlib
+    elif not crypt_algo:
+        hash = _hash_none
+    else:
+        print('Unknown password-hash %s, not hashing passwords.' % crypt_algo, file=sys.stderr)
+        hash = _hash_none
 
 ###################
 ### Redis cache ###
@@ -58,7 +98,6 @@ class RedisCache(CacheBase):
             config.getint(section, 'redis-port'),
             config.getint(section, 'redis-db')
         )
-        self.expire = config.getint(section, 'cache-expire')
 
     def check_password(self, user, password):
         """Check the given user and password.
@@ -69,10 +108,10 @@ class RedisCache(CacheBase):
         if cached is None:
             return cached
         else:
-            return cached == bytes(password, 'utf-8')
+            return cached == bytes(self.hash(password, cached.decode('utf-8')), 'utf-8')
 
     def set_password(self, user, password):
-        self.conn.set(self.prefix('%s-pass' % user), password, ex=self.expire)
+        self.conn.set(self.prefix('%s-pass' % user), self.hash(password), ex=self.expire)
 
     def in_groups(self, user, groups):
         key = self.prefix('%s-groups' % user)
@@ -97,22 +136,20 @@ class RedisCache(CacheBase):
 #######################
 class MemcachedCache(CacheBase):
     def __init__(self, config, section):
-        import hashlib
         import memcache
-        self.hashlib = hashlib
         self.conn = memcache.Client(config.get(section, 'memcache-server').split())
 
     def key(self, raw):
-        return self.prefix(self.hashlib.md5(bytes(raw, 'utf-8')).hexdigest())
+        return self.prefix(hashlib.md5(bytes(raw, 'utf-8')).hexdigest())
 
     def check_password(self, user, password):
         cached = self.conn.get(self.key('%s-pass' % user))
         if cached is None:
             return cached
-        return cached == password
+        return cached == self.hash(password, cached)
 
     def set_password(self, user, password):
-        self.conn.set(self.key('%s-pass' % user), password, self.expire)
+        self.conn.set(self.key('%s-pass' % user), self.hash(password), self.expire)
 
     def in_groups(self, user, groups):
         cached = self.conn.get(self.key('%s-groups' % user))
